d37897d1bc221a6d1f6757b81b49a327cc2793e1
chadli <68chadli@gmail.com>
2026-05-10 17:06:13 +0200

    Création auto superutilisateur

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,7 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# Créer un superutilisateur automatiquement (si non existant)
+echo "from accounts.models import User; User.objects.filter(is_superuser=True).exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+
 exec gunicorn core.wsgi:application --bind 0.0.0.0:10000
